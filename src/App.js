fd51d6ea8d7bcf3ebff2fd9bdc8135d3dac25f0f
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-03-14 10:46:52 -0400

    create react app and set up inital logic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: []
+    }
+
+    this.handleInput = this.handleInput.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.save = this.save.bind(this)
+    this.pull = this.pull.bind(this)
+  }
+
+  handleInput(e) {
+    this.setState({
+      newValue: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    let items = this.state.items
+    items.push(this.state.newValue)
+    this.setState({
+      items: items,
+      newValue: ''
+    })
+  }
+
+  save(e) {
+    e.preventDefault()
+    localStorage.setItem('jobs', JSON.stringify(this.state.items))
+  }
+
+  pull(e) {
+    e.preventDefault()
+    const jobs = localStorage.getItem('jobs')
+    if (jobs) {
+      this.setState({ items: JSON.parse(jobs) })
+    }
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Application Tracker</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleInput} />
+          <button type="submit">+</button>
+        </form>
+        {this.state.items.map(item => (
+          <div key={item}>{item}</div>
+        ))}
+        <form onSubmit={this.save}>
+          <button type="submit">Save to Storage</button>
+        </form>
+        <form onSubmit={this.pull}>
+          <button type="submit">Pull from Storage</button>
+        </form>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
