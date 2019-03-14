7cf7c329d5fc9b5aef8d1175cd5b61cf3541dfbb
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-03-14 11:04:22 -0400

    set up router and remove unneeded files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import New from './views/New'
 import './App.css'
 
 class App extends Component {
@@ -7,40 +9,6 @@ class App extends Component {
     this.state = {
       items: []
     }
-
-    this.handleInput = this.handleInput.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.save = this.save.bind(this)
-    this.pull = this.pull.bind(this)
-  }
-
-  handleInput(e) {
-    this.setState({
-      newValue: e.target.value
-    })
-  }
-
-  handleSubmit(e) {
-    e.preventDefault()
-    let items = this.state.items
-    items.push(this.state.newValue)
-    this.setState({
-      items: items,
-      newValue: ''
-    })
-  }
-
-  save(e) {
-    e.preventDefault()
-    localStorage.setItem('jobs', JSON.stringify(this.state.items))
-  }
-
-  pull(e) {
-    e.preventDefault()
-    const jobs = localStorage.getItem('jobs')
-    if (jobs) {
-      this.setState({ items: JSON.parse(jobs) })
-    }
   }
 
   render() {
@@ -48,19 +16,15 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Application Tracker</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" onChange={this.handleInput} />
-          <button type="submit">+</button>
-        </form>
-        {this.state.items.map(item => (
-          <div key={item}>{item}</div>
-        ))}
-        <form onSubmit={this.save}>
-          <button type="submit">Save to Storage</button>
-        </form>
-        <form onSubmit={this.pull}>
-          <button type="submit">Pull from Storage</button>
-        </form>
+        <nav>
+          <Link to="/">Index</Link>
+          <Link to="/new">New</Link>
+        </nav>
+        <main>
+          <Switch>
+            <Route path="/new" render={props => <New {...props} />} />
+          </Switch>
+        </main>
       </div>
     )
   }
